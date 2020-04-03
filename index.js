c4aad33d1d5acb5d3c900323c9bc9ddfad49d29b
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-02 20:56:28 -0700

    Replace fs-extra with fs, mkdirp, and remove

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var sysPath = require('path');
-var fs = require('fs-extra');
+var path = require('path');
+var fs = require('fs');
+var remove = require('remove');
+var mkdirp = require('mkdirp');
 var eachSeries = require('async-each-series');
 
 function startsWith(string, start) {
@@ -7,25 +9,25 @@ function startsWith(string, start) {
 }
 
 function directory(path, callback) {
-  fs.lstat(path, function(err, stat) {
-    if (err || !stat) fs.mkdirs(path, callback);
+  fs.lstat(path, function (err, stat) {
+    if (err || !stat) mkdirp(path, callback);
     else if (!stat.isDirectory())
-      fs.remove(path, function(err) {
-        err ? callback(err) : fs.mkdirs(path, callback);
+      remove(path, function (err) {
+        err ? callback(err) : mkdirp(path, callback);
       });
     else callback();
   });
 }
 
 function file(path, contents, callback) {
-  fs.lstat(path, function(err, stat) {
-    if (err || !stat) fs.outputFile(path, contents, 'utf8', callback);
+  fs.lstat(path, function (err, stat) {
+    if (err || !stat) fs.writeFile(path, contents, 'utf8', callback);
     else if (!stat.isFile())
-      fs.remove(path, function(err) {
-        err ? callback(err) : fs.outputFile(path, contents, 'utf8', callback);
+      remove(path, function (err) {
+        err ? callback(err) : fs.writeFile(path, contents, 'utf8', callback);
       });
     else {
-      fs.readFile(path, 'utf8', function(err, existingContents) {
+      fs.readFile(path, 'utf8', function (err, existingContents) {
         if (err) callback(err);
         else if (existingContents !== contents) fs.writeFile(path, contents, 'utf8', callback);
         else callback();
@@ -35,17 +37,17 @@ function file(path, contents, callback) {
 }
 
 function symlink(target, path, callback) {
-  fs.lstat(path, function(err, stat) {
+  fs.lstat(path, function (err, stat) {
     if (err || !stat) fs.symlink(target, path, callback);
     else if (!stat.isSymbolicLink())
-      fs.remove(path, function(err) {
+      remove(path, function (err) {
         err ? callback(err) : fs.symlink(target, path, callback);
       });
     else {
-      fs.realpath(path, function(err, realpath) {
+      fs.realpath(path, function (err, realpath) {
         if (err) callback(err);
         else if (realpath !== target)
-          fs.remove(path, function(err) {
+          remove(path, function (err) {
             err ? callback(err) : fs.symlink(target, path, callback);
           });
         else callback();
@@ -57,38 +59,27 @@ function symlink(target, path, callback) {
 function generate(dir, structure, callback) {
   eachSeries(
     Object.keys(structure),
-    function(path, callback) {
-      var fullPath = sysPath.join(dir, path.split('/').join(sysPath.sep));
+    function (relativePath, callback) {
+      var fullPath = path.join(dir, relativePath.split('/').join(path.sep));
 
-      var contents = structure[path];
+      var contents = structure[relativePath];
       if (!contents) return directory(fullPath, callback);
 
-      fs.mkdirs(sysPath.dirname(fullPath), function(err) {
+      mkdirp(path.dirname(fullPath), function (err) {
         if (err) return callback(err);
 
         if (!startsWith(contents, '~')) file(fullPath, contents, callback);
-        else
-          symlink(
-            sysPath.join(
-              dir,
-              contents
-                .slice(1)
-                .split('/')
-                .join(sysPath.sep)
-            ),
-            fullPath,
-            callback
-          );
+        else symlink(path.join(dir, contents.slice(1).split('/').join(path.sep)), fullPath, callback);
       });
     },
     callback
   );
 }
-module.exports = function(dir, structure, callback) {
+module.exports = function (dir, structure, callback) {
   if (arguments.length === 3) return generate(dir, structure, callback);
 
-  return new Promise(function(resolve, reject) {
-    generate(dir, structure, function(err) {
+  return new Promise(function (resolve, reject) {
+    generate(dir, structure, function (err) {
       err ? reject(err) : resolve();
     });
   });
