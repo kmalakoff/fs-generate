8d98e6d3c157f58e8d0bf199a13b1362e8bba17a
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-27 20:43:15 -0700

    Bump version

--- index.js
+++ index.js
@@ -7,50 +7,50 @@ var eachSeries = require('async-each-series');
 var fsCompat = require('./lib/fs-compat');
 var STAT_OPTIONS = { bigint: process.platform === 'win32' };
 
-function directory(path, callback) {
-  fsCompat.lstat(path, STAT_OPTIONS, function (err, stat) {
-    if (err || !stat) mkpath(path, callback);
-    else if (!stat.isDirectory())
-      rimraf(path, function (err) {
-        err ? callback(err) : mkpath(path, callback);
+function directory(fullPath, callback) {
+  fsCompat.lstat(fullPath, STAT_OPTIONS, function (err, stat) {
+    if (err || !stat) mkpath(fullPath, callback);
+    else if (!stat.isDirectory()) {
+      rimraf(fullPath, function (err) {
+        err ? callback(err) : mkpath(fullPath, callback);
       });
-    else callback();
+    } else callback();
   });
 }
 
-function file(path, contents, callback) {
-  fsCompat.lstat(path, STAT_OPTIONS, function (err, stat) {
-    if (err || !stat) fs.writeFile(path, contents, 'utf8', callback);
-    else if (!stat.isFile())
-      rimraf(path, function (err) {
-        err ? callback(err) : fs.writeFile(path, contents, 'utf8', callback);
+function file(fullPath, contents, callback) {
+  fsCompat.lstat(fullPath, STAT_OPTIONS, function (err, stat) {
+    if (err || !stat) fs.writeFile(fullPath, contents, 'utf8', callback);
+    else if (!stat.isFile()) {
+      rimraf(fullPath, function (err) {
+        err ? callback(err) : fs.writeFile(fullPath, contents, 'utf8', callback);
       });
-    else {
-      fs.readFile(path, 'utf8', function (err, existingContents) {
+    } else {
+      fs.readFile(fullPath, 'utf8', function (err, existingContents) {
         if (err) callback(err);
-        else if (existingContents !== contents) fs.writeFile(path, contents, 'utf8', callback);
+        else if (existingContents !== contents) fs.writeFile(fullPath, contents, 'utf8', callback);
         else callback();
       });
     }
   });
 }
 
-function symlink(target, path, callback) {
+function symlink(target, fullPath, callback) {
   fsCompat.lstatReal(target, STAT_OPTIONS, function (err, targetStat) {
     if (err || !targetStat) return callback(err || new Error('Symlink path does not exist' + target));
     var type = targetStat.isDirectory() ? 'dir' : 'file';
 
-    fsCompat.lstat(path, STAT_OPTIONS, function (err, stat) {
-      if (err || !stat) fs.symlink(target, path, type, callback);
-      else if (!stat.isSymbolicLink())
-        rimraf(path, function (err) {
-          err ? callback(err) : fs.symlink(target, path, type, callback);
+    fsCompat.lstat(fullPath, STAT_OPTIONS, function (err, stat) {
+      if (err || !stat) fs.symlink(target, fullPath, type, callback);
+      else if (!stat.isSymbolicLink()) {
+        rimraf(fullPath, function (err) {
+          err ? callback(err) : fs.symlink(target, fullPath, type, callback);
         });
-      else {
-        fsCompat.realpath(path, function (err, realpath) {
+      } else {
+        fsCompat.realpath(fullPath, function (err, realpath) {
           if (err || realpath !== target)
-            rimraf(path, function (err) {
-              err ? callback(err) : fs.symlink(target, path, type, callback);
+            rimraf(fullPath, function (err) {
+              err ? callback(err) : fs.symlink(target, fullPath, type, callback);
             });
           else callback();
         });
@@ -78,9 +78,9 @@ function generate(dir, structure, callback) {
     callback
   );
 }
+
 module.exports = function (dir, structure, callback) {
   if (arguments.length === 3) return generate(dir, structure, callback);
-
   return new Promise(function (resolve, reject) {
     generate(dir, structure, function (err) {
       err ? reject(err) : resolve();
