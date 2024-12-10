4902bed6ed6a7188d6fadb69b8a8943aa2a5edca
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-09 21:38:08 -0800

    Upgrade modules

--- dist/cjs/fs-compat/lstatReal.js
+++ dist/cjs/fs-compat/lstatReal.js
@@ -7,4 +7,4 @@ module.exports = function lstatReal(path, options, callback) {
         lstat(realpath, options, callback);
     });
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
