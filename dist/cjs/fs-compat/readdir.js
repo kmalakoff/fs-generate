376cb5abd87fb73f87d5b366ae3b1909a07843b5
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-10 18:09:17 -0800

    Upgrade modules

--- dist/cjs/fs-compat/readdir.js
+++ dist/cjs/fs-compat/readdir.js
@@ -13,4 +13,4 @@ function readdirAddOptions(path, _options, callback) {
     return readdir(path, callback);
 }
 module.exports = fs.readdir.length === 3 ? fs.readdir : readdirAddOptions;
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
--- dist/cjs/fs-compat/stat.js
+++ dist/cjs/fs-compat/stat.js
@@ -4,4 +4,4 @@ function statAddOptions(path, _options, callback) {
     return fs.stat(path, callback);
 }
 module.exports = fs.stat.length === 3 ? fs.stat : statAddOptions;
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
