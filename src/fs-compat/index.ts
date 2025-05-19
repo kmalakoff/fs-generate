1fd54f154ee7634dfe38842687c95e7348babd7a
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 13:14:37 -0700

    Upgrade to mandatory typescript extensions

---
+++ src/fs-compat/index.ts
@@ -0,0 +1,13 @@
+import lstat from './lstat.js';
+import lstatReal from './lstatReal.js';
+import readdir from './readdir.js';
+import realpath from './realpath.js';
+import stat from './stat.js';
+
+export default {
+  lstat,
+  lstatReal,
+  readdir,
+  realpath,
+  stat,
+};
